4b38b737d326840c537353bfbf4542cd27e49442
kotakbulat <hokilimpah@gmail.com>
2026-03-13 16:28:21 +0700

    feat: add image-wrapper for pseudo shadow effect

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -28,17 +28,19 @@ function Hero() {
             </div>
 
             <div className="hero-right">
-                <img    
-                    src={profile}
-                    alt="profile"
-                    className="profile-image"
-                />
+                <div className="image-wrapper">
+                        <img    
+                        src={profile}
+                        alt="profile"
+                        className="profile-image"
+                    />
+                </div>
 
                 <div className="socials">
                     <a href="https://instagram.com/urmyhoki"><MagneticTooltip text="Instagram"><Instagram /></MagneticTooltip></a>
                     <a href="https://github.com/kotakbulat"><Tooltip text="Github"><Github /></Tooltip></a>
                     <a href="https://www.linkedin.com/in/hoki-wijaya-80528b144"><Tooltip text="Linkedin"><Linkedin /></Tooltip></a>
-                    <a href="mailto:hokilimpahwijaya@gmail.com"><Tooltip text="Email"><Mail /></Tooltip></a>
+                    <a href="mailto:hokicoc@mail.ugm.ac.id"><Tooltip text="Email"><Mail /></Tooltip></a>
                     <a href="#"><Tooltip text="Resume"><FileText /></Tooltip></a>
                 </div>
             </div>
