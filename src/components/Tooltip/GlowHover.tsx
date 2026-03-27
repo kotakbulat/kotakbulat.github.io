7d45c8d85a4f96b1b6bee2825ca7ce7f9f655114
kotakbulat <hokilimpah@gmail.com>
2026-03-27 18:30:09 +0700

    fix: made the GlowHover component more responsive

--- src/components/Tooltip/GlowHover.tsx
+++ src/components/Tooltip/GlowHover.tsx
@@ -7,14 +7,31 @@ type GlowProps = {
 
 
 export function GlowHover({ children, className }: GlowProps) {
-    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-        const rect = e.currentTarget.getBoundingClientRect();
-        e.currentTarget.style.setProperty("--x", `${e.clientX - rect.left}px`);
-        e.currentTarget.style.setProperty("--y", `${e.clientY - rect.top}px`);
+
+    const updatePosition = (el: HTMLDivElement, clientX: number, clientY: number) => {
+        const rect = el.getBoundingClientRect();
+        const x = clientX - rect.left;
+        const y = clientY - rect.top;
+
+        el.style.setProperty("--x", `${x}px`);
+        el.style.setProperty("--y", `${y}px`);
     }
 
+    const handlePointerMove = (e: React.PointerEvent<HTMLDivElement>) => {
+        updatePosition(e.currentTarget, e.clientX,  e.clientY);
+    }
+
+    const handlePointerEnter = (e: React.PointerEvent<HTMLDivElement>) => {
+        updatePosition(e.currentTarget, e.clientX, e.clientY);
+    }
+
+
     return (
-        <div className={`glow-hover ${className}`} onMouseMove={handleMouseMove}>
+        <div 
+            className={`glow-hover ${className}`} 
+            onMouseMove={handlePointerMove}
+            onPointerEnter={handlePointerEnter}
+            >
             {children}
         </div>
     )
